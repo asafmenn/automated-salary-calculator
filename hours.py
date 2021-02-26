75164471a0ae8c2c0a726099bd808e0cc522546a
asafmenn <79008032+asafmenn@users.noreply.github.com>
2021-02-26 18:08:36 +0200

    Add files via upload

---
+++ hours.py
@@ -0,0 +1,32 @@
+# THIS TEXT FILES CONTAINS ALL THE LOG OF THE HOURS EVERY MONTH
+month_hours = 'HadSigns/FebHrs.txt'
+
+
+def get_salary_details(start, end):
+    h1, m1 = start.split(':')
+    h2, m2 = end.split(':')
+    h1 = int(h1)
+    h2 = int(h2)
+    m1 = int(m1)
+    m2 = int(m2)
+    time_start = h1*60 + m1
+    time_end = h2*60 + m2
+    diff = time_end - time_start
+    hours = int(diff / 60)
+    minutes = int((diff % 60)/60 * 100)
+    total_time = float(str(hours) + '.' +str(minutes))
+    return total_time
+
+
+with open(month_hours, 'r') as file:
+    work_day = 0
+    total_hours = []
+    for line in file:
+        line = line.split(' - ')
+        date, start_time, end_time = line
+        work_hours = get_salary_details(start_time, end_time)
+        total_hours.append(work_hours)
+        work_day += 1
+
+
+
---
+++ salarycalc.py
@@ -0,0 +1,23 @@
+from hours import work_day, total_hours
+base_salary = 50
+daily_hour = 10
+food_budget = 100
+total_sal = 0
+counter = 1
+days = work_day
+while days > 0:
+    for hours in total_hours:
+        if hours <= daily_hour:
+            salary = base_salary*hours + food_budget
+
+        if daily_hour < hours <= 12:
+            salary = daily_hour*base_salary + (hours-daily_hour)*1.25*base_salary + food_budget
+
+        elif hours > 12:
+            salary = daily_hour*base_salary + 2*1.25*base_salary + (hours-daily_hour-2)*1.5*base_salary + food_budget
+        total_sal += salary
+        days -= 1
+        counter += 1
+
+print("your salary is: {}".format(total_sal))
+
