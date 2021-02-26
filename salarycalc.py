fced4b48e773c53fff6039ad20ccf60a233a89d1
asafmenn <79008032+asafmenn@users.noreply.github.com>
2021-02-26 18:47:26 +0200

    Update salarycalc.py

--- salarycalc.py
+++ salarycalc.py
@@ -1,7 +1,7 @@
 from hours import work_day, total_hours
 # PARAMETERS:
 base_salary = 50
-daily_hour = 10
+daily_hour = 9
 food_budget = 100
 total_sal = 0
 counter = 1
@@ -11,14 +11,13 @@ while days > 0:
         if hours <= daily_hour:
             salary = base_salary*hours + food_budget
 
-        if daily_hour < hours <= 12:
+        elif daily_hour < hours <= 12:
             salary = daily_hour*base_salary + (hours-daily_hour)*1.25*base_salary + food_budget
 
-        elif hours > 12:
+        else:
             salary = daily_hour*base_salary + 2*1.25*base_salary + (hours-daily_hour-2)*1.5*base_salary + food_budget
         total_sal += salary
         days -= 1
         counter += 1
 
 print("your salary is: {}".format(total_sal))
-
